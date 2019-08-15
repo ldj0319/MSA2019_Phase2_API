835b72c4f084409df33f261ea667985e631d5be4
ldj0319 <ldj970319@gmail.com>
2019-08-16 03:50:06 +1200

    using my database

--- ScribrAPI/Model/scriberContext.cs
+++ ScribrAPI/Model/scriberContext.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
@@ -24,7 +24,7 @@ namespace ScribrAPI.Model
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=tcp:msascribr.database.windows.net,1433;Initial Catalog=scriber;Persist Security Info=False;User ID=XLIN928;Password=XIzz4#11cDHr;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                optionsBuilder.UseSqlServer("Server=tcp:jaemsaphase2scriber.database.windows.net,1433;Initial Catalog=scriber;Persist Security Info=False;User ID=ldj0319;Password=Knh12171;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
 
