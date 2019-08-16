fa26cc888590625d0b227de5eab20a326f6811ac
ldj0319 <ldj970319@gmail.com>
2019-08-17 01:08:49 +1200

    CORS problem

--- ScribrAPI/Controllers/TranscriptionsController.cs
+++ ScribrAPI/Controllers/TranscriptionsController.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +12,7 @@ namespace ScribrAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [EnableCors("MyPolicy")]
     public class TranscriptionsController : ControllerBase
     {
         private readonly scriberContext _context;
