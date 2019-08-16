ac771fe997756ea8e9fd235d1dbf7a9be5acb6f9
ldj0319 <ldj970319@gmail.com>
2019-08-16 22:39:21 +1200

    local

--- ScribrAPI/Startup.cs
+++ ScribrAPI/Startup.cs
@@ -76,7 +76,7 @@ namespace ScribrAPI
             // Make sure the CORS middleware is ahead of SignalR.
             app.UseCors(builder =>
             {
-                builder.WithOrigins("http://localhost:3000/")
+                builder.WithOrigins("http://localhost:3000")
                     .AllowAnyHeader()
                     .AllowAnyMethod()
                     .AllowCredentials();
